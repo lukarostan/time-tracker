1e855af416bc68f5e004121f457e62df686d7847
lukarostan <luka.rostan@undabot.com>
2024-01-06 20:38:21 +0100

    Add app pages

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -4,15 +4,15 @@ import React, {ReactElement} from 'react';
 import Header from '@/components/Header';
 import {PrimeReactProvider} from 'primereact/api';
 import 'primereact/resources/themes/saga-blue/theme.css';
-import LoginForm from '@/components/LoginForm';
-import {login} from '@/services/AuthorizationService';
+import {ActiveTrackers} from '@/components/ActiveTrackers';
+import {History} from '@/components/History';
 
 export default function Home(): ReactElement {
     return (
         <PrimeReactProvider>
             <main style={{width:'100%', display: 'flex', flexDirection: 'column'}}>
                 <Header/>
-                <LoginForm onLoginClick={login}/>
+                <History/>
             </main>
         </PrimeReactProvider>
     );
